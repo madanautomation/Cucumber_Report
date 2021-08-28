0de4d55ace01ddd5e3f027c6f3da77401194deed
madan <madanreddy319@gmail.com>
2021-08-28 20:22:16 +0100

    report

---
+++ Report/report.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/features/Login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Admin Login",
+  "description": "",
+  "id": "admin-login",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 4921710558,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Enter the education form",
+  "description": "",
+  "id": "admin-login;enter-the-education-form",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter the name a Primary_School",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter the name a Secondary_School",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter the name a Intermediate",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter the name a Engineering",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter the name a University",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "I enter the intermediate_Percentage",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "I click on gender",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.i_enter_the_name_a_Primary_School()"
+});
+formatter.result({
+  "duration": 343636923,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Steps.i_enter_the_name_a_Secondary_School()"
+});
+formatter.result({
+  "duration": 88411342,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Steps.i_enter_the_name_a_Intermediate()"
+});
+formatter.result({
+  "duration": 90969920,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Steps.i_enter_the_name_a_Engineering()"
+});
+formatter.result({
+  "duration": 80043384,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Steps.i_enter_the_name_a_University()"
+});
+formatter.result({
+  "duration": 101265146,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Steps.i_enter_the_intermediate_Percentage()"
+});
+formatter.result({
+  "duration": 4234829058,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Steps.i_click_on_gender()"
+});
+formatter.result({
+  "duration": 72315187,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 141131336,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 9731818,
+  "status": "passed"
+});
+});
